b71267ece245b1b40b8113dc75705ad2032054be
thejapa681 <123478768+thejapa681@users.noreply.github.com>
2026-01-13 21:19:49 -0300

    Atualizar o page.tsx

--- app/admin/(dashboard)/produtos/novo/page.tsx
+++ app/admin/(dashboard)/produtos/novo/page.tsx
@@ -136,7 +136,7 @@ try {
     colors: colors.length > 0 ? colors : null,
     sizes: sizes.length > 0 ? sizes : null,
   })
-  .select("product_id")
+  .select("*")
   .single()
 
 if (productError || !product) throw productError || new Error("Erro ao criar produto")
